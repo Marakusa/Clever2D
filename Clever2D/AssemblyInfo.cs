dab764dbbfdefbf61efd981d8599f87964ab62a5
Markus <29477753+Marakusa@users.noreply.github.com>
2021-10-02 00:23:16 +0300

    Update AssemblyInfo.cs

--- Clever2D/AssemblyInfo.cs
+++ Clever2D/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET based 2D game engine written in C#")]
 [assembly: AssemblyCompany("Aquatic Studios")]
 [assembly: AssemblyProduct("Clever2D")]
-[assembly: AssemblyCopyright("Aquatic Studios © 2021")]
+[assembly: AssemblyCopyright("Aquatic Studios ï¿½ 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2021.1.1.1")]
+[assembly: AssemblyVersion("2021.2.1.1")]
 
 // In SDK-style projects such as this one, several assembly attributes that were historically
 // defined in this file are now automatically added during build and populated with
