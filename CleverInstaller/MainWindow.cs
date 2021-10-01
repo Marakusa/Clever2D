56760315db08f1c34b6491570ab5343ed9c7d918
Markus <29477753+Marakusa@users.noreply.github.com>
2021-10-02 00:19:25 +0300

    Added isntaller

---
+++ CleverInstaller/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Reflection;
+using System.Diagnostics;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+using Cairo;
+
+namespace CleverInstaller
+{
+    class MainWindow : Window
+    {
+        [UI] private Label _label1 = null;
+        [UI] private Button _button1 = null;
+
+        private int _counter;
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+            _button1.Clicked += Button1_Clicked;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+
+        private void Button1_Clicked(object sender, EventArgs a)
+        {
+            _counter++;
+            _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
+        }
+    }
+}
