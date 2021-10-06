67c27137cca343ceab2c329cfa84e39b43575791
Markus <29477753+Marakusa@users.noreply.github.com>
2021-10-06 17:34:59 +0300

    Added editor layout

---
+++ Clever2D.Editor/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using Clever2D.Engine;
+using Gtk;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace Clever2D.Editor
+{
+    class MainWindow : Window
+    {
+        private int _counter;
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Gtk.Application.Quit();
+        }
+    }
+}
